cd5465028469a3fe6a73c617807e37dc7fef0de9
Shihab Uddin Khan <aimshihabuddinkhan@gmail.com>
2023-10-07 21:06:16 +0000

    Fix changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  # get 'root/index'
-  # get 'greetings/random'
-  # # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
     resources :greetings, only: :index
    
    end
   #  greetings#index
    root 'root#index'
-  # # Defines the root path route ("/")
-  # get 'greeting', to: '/api/greetings#index'
-  # # root "articles#index"
 end
