74241e9f954eaf2ff8440409a7622bfe862b9fe7
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-10-08 01:26:26 +0600

    Add Controller Model Views

---
+++ app/javascript/application.js
@@ -0,0 +1,18 @@
+// Entry point for the build script in your package.json
+// import "@hotwired/turbo-rails"
+// import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import App from './components/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+  <Provider store={store}>
+      <App />
+      </Provider>
+  </BrowserRouter>,
+);
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # get 'root/index'
+  # get 'greetings/random'
+  # # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :greetings, only: :index
+   
+   end
+  #  greetings#index
+   root 'root#index'
+  # # Defines the root path route ("/")
+  # get 'greeting', to: '/api/greetings#index'
+  # # root "articles#index"
 end
